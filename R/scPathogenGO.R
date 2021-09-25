8fed45534e4e45edb94633e7f9a5ff4a0aa55be4
ncrna <ncrna@foxmail.com>
2021-09-26 01:05:35 +0800

    Update

--- R/scPathogenGO.R
+++ R/scPathogenGO.R
@@ -5,110 +5,116 @@
 #' @param clusters A vector of clusters, default all clusters will be used
 #' @param species Species which is used to divid cells into two groups
 #' @param logFC A positive value to set the cutoff of logFC
-#' @param only.pos Only up-regulated genes are taken into account, default only.pos=TRUE
-#' @param reference Reference organism to set ['human' or 'mouse'], default reference = 'human'
+#' @param only.pos Only up-regulated genes are taken into account, 
+#'   default only.pos=TRUE
+#' @param reference Reference organism to set ['human' or 'mouse'], 
+#'   default reference = 'human'
 #' @return Table that stores scPathogenGO results
 #'
 #' @author Wei Zhang
 #' @export
 
-scPathogenGO <- function(object = NULL, key = NULL, species = NULL, clusters = NULL,
-    logFC = 0.25, only.pos = TRUE, reference = "human") {
-    avg_log2FC <- p_val_adj <- Data <- cluster <- NULL
-    if (is.null(object)) {
-        stop("Parameter 'object' must be specified!\n")
+scPathogenGO <- function(object = NULL, key = NULL, species = NULL, 
+                         clusters = NULL, logFC = 0.25, only.pos = TRUE, 
+                         reference = "human") {
+  avg_log2FC <- p_val_adj <- Data <- cluster <- NULL
+  if (is.null(object)) {
+    stop("Parameter 'object' must be specified!\n")
+  }
+  if (is.null(key)) {
+    stop("Parameter 'key' must be specified!\n")
+  }
+  if (!key %in% names(object@misc)) {
+    stop("The 'key' ", key, " does not exist!\n")
+  }
+  if (is.null(species)) {
+    stop("Parameter 'species' must be specified!\n")
+  }
+  if (!species %in% names(object@misc[[key]])) {
+    stop("The 'species' ", species, " does not exist!\n")
+  }
+  if (is.null(clusters)) {
+    warning("All clusters will be evaluated!\n")
+    clusters <- levels(object)
+  }
+  clusters <- as.character(clusters)
+  direction <- c("up", "down")
+  if (only.pos) {
+    directions <- c("up")
+  }
+  x <- list()
+  mappings <- NULL
+  if (reference == "human") {
+    x <- as.list(org.Hs.eg.db::org.Hs.egALIAS2EG)
+    mappings <- "org.Hs.eg.db"
+  } else if (reference == "mouse") {
+    x <- as.list(org.Mm.eg.db::org.Mm.egALIAS2EG)
+    mappings <- "org.Mm.eg.db"
+  } else {
+    stop("Parameter 'reference' must be specified with 'human' or 'mouse'!\n")
+  }
+  geneList <- rep(0, length(rownames(object)))
+  names(geneList) <- rownames(object)
+  geneList <- geneList[intersect(names(geneList), names(x))]
+  TotalGenes <- names(geneList)
+  for (ii in seq_len(length(geneList))) {
+    names(geneList)[ii] = x[[names(geneList)[ii]]][1]
+  }
+  go_enrichment_results = list()
+  go_enrichment_results[[species]] = list()
+  Data = object@misc[[key]][[species]]
+  # Fix the header of the DGE result with early version of 'MAST' package
+  if ("avg_logFC" %in% colnames(Data)) {
+    colnames(Data)[which(colnames(Data) == "avg_logFC")] <- "avg_log2FC"
+  }
+  for (C in clusters) {
+    DEdata <- subset(Data, cluster == C)
+    if (nrow(DEdata) == 0) {
+      warning("No DE genes between species ", species, " pos/neg in cluster-", C)
+      next
     }
-    if (is.null(key)) {
-        stop("Parameter 'key' must be specified!\n")
-    }
-    if (!key %in% names(object@misc)) {
-        stop("The 'key' ", key, " does not exist!\n")
-    }
-    if (is.null(species)) {
-        stop("Parameter 'species' must be specified!\n")
-    }
-    if (!species %in% names(object@misc[[key]])) {
-        stop("The 'species' ", species, " does not exist!\n")
-    }
-    if (is.null(clusters)) {
-        warning("All clusters will be evaluated!\n")
-        clusters <- levels(object)
-    }
-    clusters <- as.character(clusters)
-    direction <- c("up", "down")
-    if (only.pos) {
-        directions <- c("up")
-    }
-    x <- list()
-    mappings <- NULL
-    if (reference == "human") {
-        x <- as.list(org.Hs.eg.db::org.Hs.egALIAS2EG)
-        mappings <- "org.Hs.eg.db"
-    } else if (reference == "mouse") {
-        x <- as.list(org.Mm.eg.db::org.Mm.egALIAS2EG)
-        mappings <- "org.Mm.eg.db"
-    } else {
-        stop("Parameter 'reference' must be specified with 'human' or 'mouse'!\n")
-    }
-    geneList <- rep(0, length(rownames(object)))
-    names(geneList) <- rownames(object)
-    geneList <- geneList[intersect(names(geneList), names(x))]
-    TotalGenes <- names(geneList)
-    for (ii in seq_len(length(geneList))) {
-        names(geneList)[ii] = x[[names(geneList)[ii]]][1]
-    }
-    go_enrichment_results = list()
-    go_enrichment_results[[species]] = list()
-    Data = object@misc[[key]][[species]]
-    # Fix the header of the DGE result with early version of 'MAST' package
-    if ("avg_logFC" %in% colnames(Data)) {
-        colnames(Data)[which(colnames(Data) == "avg_logFC")] <- "avg_log2FC"
-    }
-    for (C in clusters) {
-        DEdata <- subset(Data, cluster == C)
-        if (nrow(DEdata) == 0) {
-            warning("No DE genes between species ", species, " pos/neg in cluster-",
-                C)
-            next
-        }
-        go_enrichment_results[[species]][[C]] = list()
-        print(paste0("Running ", species, " in cluster-", C, " ... "))
+    go_enrichment_results[[species]][[C]] = list()
 
-        for (direction in c("up", "down")) {
-            if (direction == "up") {
-                queryMatrix = subset(DEdata[order(DEdata$pct.1/DEdata$pct.2, decreasing = TRUE),
-                  ], avg_log2FC > abs(logFC) & p_val_adj < 0.01)
-            } else {
-                queryMatrix = subset(DEdata[order(DEdata$pct.2/DEdata$pct.1, decreasing = TRUE),
-                  ], avg_log2FC < -abs(logFC) & p_val_adj < 0.01)
-            }
-            queryGene = rownames(queryMatrix)
-            # next if queryGene with only one Gene
-            if (length(queryGene) < 2) {
-                next
-            }
-            # Run against topGO ####
-            queryGeneList = geneList
-            queryGeneList[which(TotalGenes %in% queryGene)] = 1
-            if (length(table(queryGeneList)) < 2) {
-                next
-            }
-            queryMatrix$gene = rownames(queryMatrix)
-            queryGene = queryMatrix$gene
-            go_enrichment_results[[species]][[C]][[direction]] = list()
+    message("Running ", species, " in cluster-", C, " ... ")
 
-            tab = list()
-            for (ont in c("BP", "CC", "MF")) {
-                GOdata <- suppressMessages(new("topGOdata", ontology = ont, allGenes = as.factor(queryGeneList),
-                  nodeSize = 10, annot = topGO::annFUN.org, mapping = mappings, ID = "entrez"))
-                resultTopGO.elim <- suppressMessages(topGO::runTest(GOdata, algorithm = "elim",
-                  statistic = "Fisher"))
-                tab[[ont]] <- rbind(tab[[ont]], topGO::GenTable(GOdata, Fisher.elim = resultTopGO.elim,
-                  orderBy = "Fisher.elim", topNodes = 200))
-            }
-            go_enrichment_results[[species]][[C]][[direction]] = tab
-        }
-        print("done.\n")
+    for (direction in c("up", "down")) {
+      if (direction == "up") {
+        queryMatrix = subset(DEdata[order(DEdata$pct.1/DEdata$pct.2, 
+          decreasing = TRUE), ], avg_log2FC > abs(logFC) & p_val_adj < 0.01)
+      } else {
+        queryMatrix = subset(DEdata[order(DEdata$pct.2/DEdata$pct.1, 
+          decreasing = TRUE), ], avg_log2FC < -abs(logFC) & p_val_adj < 0.01)
+      }
+      queryGene = rownames(queryMatrix)
+      # next if queryGene with only one Gene
+      if (length(queryGene) < 2) {
+        next
+      }
+      # Run against topGO ####
+      queryGeneList = geneList
+      queryGeneList[which(TotalGenes %in% queryGene)] = 1
+      if (length(table(queryGeneList)) < 2) {
+        next
+      }
+      queryMatrix$gene = rownames(queryMatrix)
+      queryGene = queryMatrix$gene
+      go_enrichment_results[[species]][[C]][[direction]] = list()
+
+      tab = list()
+      for (ont in c("BP", "CC", "MF")) {
+        GOdata <- suppressMessages(new("topGOdata", ontology = ont, 
+          allGenes = as.factor(queryGeneList),
+          nodeSize = 10, annot = topGO::annFUN.org, mapping = mappings, 
+          ID = "entrez"))
+        resultTopGO.elim <- suppressMessages(topGO::runTest(GOdata, 
+          algorithm = "elim", statistic = "Fisher"))
+        tab[[ont]] <- rbind(tab[[ont]], topGO::GenTable(GOdata, 
+          Fisher.elim = resultTopGO.elim,
+          orderBy = "Fisher.elim", topNodes = 200))
+      }
+      go_enrichment_results[[species]][[C]][[direction]] = tab
     }
-    return(go_enrichment_results)
+    message("done.\n")
+  }
+  return(go_enrichment_results)
 }
--- R/scVizMeta.R
+++ R/scVizMeta.R
@@ -4,7 +4,7 @@
 #' @param object Seurat object
 #' @param signature Name of one gene set
 #' @param pt.size Adjust point size to plot, default pt.size=0.5
-#' @param reduction\tWhich dimensionality reduction to use, default is 'umap'
+#' @param reduction Which dimensionality reduction to use, default is 'umap'
 #' @param title Text for the plot title, default title=NULL
 #' @param split.by Name of a metadata column to split plot by
 #' @param ncol Number of columns for display the plots
@@ -20,132 +20,138 @@
 #' @author Wei Zhang
 #' @export
 
-scVizMeta <- function(object = NULL, signature = NULL, pt.size = NULL, reduction = NULL,
-    title = NULL, split.by = NULL, ncol = NULL, raster = TRUE, palette = "YlOrRd",
-    interval = c(Max = 1e+05, Hyper = 10000, Abundant = 1000, Large = 500, Medium = 100,
-        Small = 10, Single = 1, None = 0)) {
-    if (is.null(signature)) {
-        stop("Parameter 'signature' must be specified!\n")
-    } else if (length(signature) != 1) {
-        stop("Parameter 'signature' must be one pathway!\n")
+scVizMeta <- function(object = NULL, signature = NULL, pt.size = NULL, 
+                      reduction = NULL, title = NULL, split.by = NULL, 
+                      ncol = NULL, raster = TRUE, palette = "YlOrRd",
+                      interval = c(Max = 1e+05, Hyper = 10000, 
+                                   Abundant = 1000, Large = 500, 
+                                   Medium = 100, Small = 10, 
+                                   Single = 1, None = 0)) {
+  if (is.null(signature)) {
+    stop("Parameter 'signature' must be specified!\n")
+  } else if (length(signature) != 1) {
+    stop("Parameter 'signature' must be one pathway!\n")
+  }
+  if (!signature %in% names(object@meta.data)) {
+    stop("The signature parameter does not exist in MetaData slot!\n")
+  }
+  if (is.null(pt.size)) {
+    pt.size <- 0.5
+  }
+  if (is.null(reduction)) {
+    if ("umap" %in% names(object)) {
+      reduction <- "umap"
+    } else if ("tsne" %in% names(object)) {
+      reduction <- "tsne"
+    } else if ("pca" %in% names(object)) {
+      reduction <- "pca"
+    } else {
+      stop("The reduction parameter does not support! Please use 'umap', 'tsne', or 'pca' instead.\n")
     }
-    if (!signature %in% names(object@meta.data)) {
-        stop("The signature parameter does not exist in MetaData slot!\n")
+  }
+  for (x in seq_along(interval)) {
+    names(interval)[x] <- paste0(names(interval[x]), " (", interval[x + 1], 
+      " < X <= ", interval[x], ")")
+  }
+  names(interval)[length(interval)] <- gsub("\\(NA < X <", "\\(X", names(interval)[length(interval)])
+  xmin <- xmax <- ymin <- ymax <- NULL
+  ps <- function(data, cols, title = NULL, legend_title = NULL) {
+    p <- ggplot2::ggplot(data = data, ggplot2::aes(x = data[, 1], y = data[, 2]))
+    if (isTRUE(raster)) {
+      p <- p + ggrastr::rasterise(dpi = 300, 
+        ggplot2::geom_point(ggplot2::aes(colour = class), 
+          size = pt.size, na.rm = TRUE))
+    } else {
+    p <- p + ggplot2::geom_point(ggplot2::aes(colour = class), 
+      size = pt.size, na.rm = TRUE)
     }
-    if (is.null(pt.size)) {
-        pt.size <- 0.5
+    p <- p + cowplot::theme_cowplot()
+    p <- p + ggplot2::guides(colour = ggplot2::guide_legend(override.aes = list(size = 5)))
+    p <- p + ggplot2::scale_color_manual(values = cols)
+    p <- p + ggplot2::labs(x = colnames(data)[1], y = colnames(data)[2], title = title)
+    p <- p + ggplot2::scale_x_continuous(limits = c(xmin, xmax), 
+      breaks = seq(floor(xmin/5) * 5, ceiling(xmax/5) * 5, by = 5))
+    p <- p + ggplot2::scale_y_continuous(limits = c(ymin, ymax), 
+      breaks = seq(floor(ymin/5) * 5, ceiling(ymax/5) * 5, by = 5))
+    return(p)
+  }
+  pm <- function(data, cols = cols, min.value = min.value, 
+    max.value = max.value, title = NULL, legend_title = NULL) {
+    p <- ggplot2::ggplot(data = data, ggplot2::aes(x = data[, 1], y = data[, 2]))
+    if (isTRUE(raster)) {
+      p <- p + ggrastr::rasterise(dpi = 300, 
+        ggplot2::geom_point(ggplot2::aes(colour = class), 
+          size = pt.size, na.rm = TRUE))
+    } else {
+      p <- p + ggplot2::geom_point(ggplot2::aes(colour = class), 
+        size = pt.size, na.rm = TRUE)
     }
-    if (is.null(reduction)) {
-        if ("umap" %in% names(object)) {
-            reduction <- "umap"
-        } else if ("tsne" %in% names(object)) {
-            reduction <- "tsne"
-        } else if ("pca" %in% names(object)) {
-            reduction <- "pca"
-        } else {
-            stop("The reduction parameter does not support! Please use 'umap', 'tsne', or 'pca' instead.\n")
-        }
+    p <- p + cowplot::theme_cowplot() + 
+      ggplot2::guides(colour = ggplot2::guide_legend(override.aes = list(size = 5))) +
+      ggplot2::scale_color_manual(values = cols)
+    p <- p + ggplot2::labs(x = colnames(data)[1], y = colnames(data)[2], title = title)
+    p <- p + ggplot2::scale_x_continuous(limits = c(xmin, xmax), 
+      breaks = seq(floor(xmin/5) * 5, ceiling(xmax/5) * 5, by = 5))
+    p <- p + ggplot2::scale_y_continuous(limits = c(ymin, ymax), 
+      breaks = seq(floor(ymin/5) * 5, ceiling(ymax/5) * 5, by = 5))
+    p <- p + ggplot2::theme(legend.position = "none")
+    return(p)
+  }
+  reduction_ids <- gsub("coord", toupper(reduction), c("coord_1", "coord_2"))
+  if (is.null(x = split.by)) {
+    Data <- object@meta.data[, c(reduction_ids, signature)]
+    Data[, signature][is.na(Data[, signature])] <- 0
+    Data[, "class"] <- NA
+    for (i in seq_len(length(interval) - 1)) {
+      Data$class <- ifelse(Data[, signature] > interval[i + 1] & 
+        Data[, signature] <= interval[i], names(interval[i]), Data$class)
     }
-    for (x in seq_along(interval)) {
-        names(interval)[x] <- paste0(names(interval[x]), " (", interval[x + 1], " < X <= ",
-            interval[x], ")")
+    Data$class[is.na(Data$class)] <- names(interval)[length(interval)]
+    Data$class <- factor(Data$class, 
+      levels = intersect(names(interval), unique(Data$class)))
+    Data <- Data[order(Data[, signature]), ]
+    xmin <- min(Data[, 1])
+    xmax <- max(Data[, 1])
+    ymin <- min(Data[, 2])
+    ymax <- max(Data[, 2])
+    cols <- c(rev(RColorBrewer::brewer.pal(length(levels(Data$class)) - 1, 
+      palette))[1:length(levels(Data$class)) -1], "gray")
+    # names(cols) <- names(interval)
+    names(cols) <- names(interval)[(length(interval) - length(levels(Data$class)) + 1):length(interval)]
+    return(ps(data = Data, cols = cols, title = title, legend_title = signature))
+  }
+  plots <- list()
+  if (!is.null(x = split.by)) {
+    if (!split.by %in% colnames(object@meta.data)) {
+      stop("The parameter 'split.by' ", split.by, " does not exist in MetaData slot!\n")
     }
-    names(interval)[length(interval)] <- gsub("\\(NA < X <", "\\(X", names(interval)[length(interval)])
-    xmin <- xmax <- ymin <- ymax <- NULL
-    ps <- function(data, cols, title = NULL, legend_title = NULL) {
-        p <- ggplot2::ggplot(data = data, ggplot2::aes(x = data[, 1], y = data[,
-            2]))
-        if (isTRUE(raster)) {
-            p <- p + ggrastr::rasterise(dpi = 300, ggplot2::geom_point(ggplot2::aes(colour = class),
-                size = pt.size, na.rm = TRUE))
-        } else {
-            p <- p + ggplot2::geom_point(ggplot2::aes(colour = class), size = pt.size,
-                na.rm = TRUE)
-        }
-        p <- p + cowplot::theme_cowplot() + ggplot2::guides(colour = ggplot2::guide_legend(override.aes = list(size = 5))) +
-            ggplot2::scale_color_manual(values = cols) + ggplot2::labs(x = colnames(data)[1],
-            y = colnames(data)[2], title = title)
-        p <- p + ggplot2::scale_x_continuous(limits = c(xmin, xmax), breaks = seq(floor(xmin/5) *
-            5, ceiling(xmax/5) * 5, by = 5)) + ggplot2::scale_y_continuous(limits = c(ymin,
-            ymax), breaks = seq(floor(ymin/5) * 5, ceiling(ymax/5) * 5, by = 5))
-        return(p)
+    Data <- object@meta.data[, c(reduction_ids, signature, split.by)]
+    Data[, signature][is.na(Data[, signature])] <- 0
+    Data[, "class"] <- NA
+    for (i in seq_len(length(interval) - 1)) {
+      Data$class <- ifelse(Data[, signature] > interval[i + 1] & 
+        Data[, signature] <= interval[i], names(interval[i]), Data$class)
     }
-    pm <- function(data, cols = cols, min.value = min.value, max.value = max.value,
-        title = NULL, legend_title = NULL) {
-        p <- ggplot2::ggplot(data = data, ggplot2::aes(x = data[, 1], y = data[,
-            2]))
-        if (isTRUE(raster)) {
-            p <- p + ggrastr::rasterise(dpi = 300, ggplot2::geom_point(ggplot2::aes(colour = class),
-                size = pt.size, na.rm = TRUE))
-        } else {
-            p <- p + ggplot2::geom_point(ggplot2::aes(colour = class), size = pt.size,
-                na.rm = TRUE)
-        }
-        p <- p + cowplot::theme_cowplot() + ggplot2::guides(colour = ggplot2::guide_legend(override.aes = list(size = 5))) +
-            ggplot2::scale_color_manual(values = cols) + ggplot2::labs(x = colnames(data)[1],
-            y = colnames(data)[2], title = title)
-        p <- p + ggplot2::scale_x_continuous(limits = c(xmin, xmax), breaks = seq(floor(xmin/5) *
-            5, ceiling(xmax/5) * 5, by = 5)) + ggplot2::scale_y_continuous(limits = c(ymin,
-            ymax), breaks = seq(floor(ymin/5) * 5, ceiling(ymax/5) * 5, by = 5))
-        p <- p + ggplot2::theme(legend.position = "none")
-        return(p)
+    Data$class[is.na(Data$class)] <- names(interval)[length(interval)]
+    Data$class <- factor(Data$class, levels = intersect(names(interval), unique(Data$class)))
+    Data <- Data[order(Data[, signature]), ]
+    xmin <- min(Data[, 1])
+    xmax <- max(Data[, 1])
+    ymin <- min(Data[, 2])
+    ymax <- max(Data[, 2])
+    cols <- c(rev(RColorBrewer::brewer.pal(length(levels(Data$class)) - 1, palette))[1:length(levels(Data$class)) -1], "gray")
+    names(cols) <- names(interval)
+    legend <- ps(data = Data, cols = cols, title = NULL, legend_title = signature)
+    legend <- ggplot2::ggplotGrob(legend)
+    legend <- gtable::gtable_filter(legend, "box", trim = FALSE)  # use trim depending on need
+    if (is.null(ncol)) {
+      ncol = ceiling(sqrt(length(unique(Data[, split.by]))))
     }
-    reduction_ids <- gsub("coord", toupper(reduction), c("coord_1", "coord_2"))
-    if (is.null(x = split.by)) {
-        Data <- object@meta.data[, c(reduction_ids, signature)]
-        Data[, signature][is.na(Data[, signature])] <- 0
-        Data[, "class"] <- NA
-        for (i in seq_len(length(interval) - 1)) {
-            Data$class <- ifelse(Data[, signature] > interval[i + 1] & Data[, signature] <=
-                interval[i], names(interval[i]), Data$class)
-        }
-        Data$class[is.na(Data$class)] <- names(interval)[length(interval)]
-        Data$class <- factor(Data$class, levels = intersect(names(interval), unique(Data$class)))
-        Data <- Data[order(Data[, signature]), ]
-        xmin <- min(Data[, 1])
-        xmax <- max(Data[, 1])
-        ymin <- min(Data[, 2])
-        ymax <- max(Data[, 2])
-        cols <- c(rev(RColorBrewer::brewer.pal(length(levels(Data$class)) - 1, palette))[1:length(levels(Data$class)) -
-            1], "gray")
-        # names(cols) <- names(interval)
-        names(cols) <- names(interval)[(length(interval) - length(levels(Data$class)) +
-            1):length(interval)]
-        return(ps(data = Data, cols = cols, title = title, legend_title = signature))
+    for (s in unique(Data[, split.by])) {
+      data <- Data[Data[, split.by] == s, ]
+      plots[[s]] <- pm(data = data, cols = cols, title = s, legend_title = signature)
     }
-    plots <- list()
-    if (!is.null(x = split.by)) {
-        if (!split.by %in% colnames(object@meta.data)) {
-            stop("The parameter 'split.by' ", split.by, " does not exist in MetaData slot!\n")
-        }
-        Data <- object@meta.data[, c(reduction_ids, signature, split.by)]
-        Data[, signature][is.na(Data[, signature])] <- 0
-        Data[, "class"] <- NA
-        for (i in seq_len(length(interval) - 1)) {
-            Data$class <- ifelse(Data[, signature] > interval[i + 1] & Data[, signature] <=
-                interval[i], names(interval[i]), Data$class)
-        }
-        Data$class[is.na(Data$class)] <- names(interval)[length(interval)]
-        Data$class <- factor(Data$class, levels = intersect(names(interval), unique(Data$class)))
-        Data <- Data[order(Data[, signature]), ]
-        xmin <- min(Data[, 1])
-        xmax <- max(Data[, 1])
-        ymin <- min(Data[, 2])
-        ymax <- max(Data[, 2])
-        cols <- c(rev(RColorBrewer::brewer.pal(length(levels(Data$class)) - 1, palette))[1:length(levels(Data$class)) -
-            1], "gray")
-        names(cols) <- names(interval)
-        legend <- ps(data = Data, cols = cols, title = NULL, legend_title = signature)
-        legend <- ggplot2::ggplotGrob(legend)
-        legend <- gtable::gtable_filter(legend, "box", trim = FALSE)  # use trim depending on need
-        if (is.null(ncol)) {
-            ncol = ceiling(sqrt(length(unique(Data[, split.by]))))
-        }
-        for (s in unique(Data[, split.by])) {
-            data <- Data[Data[, split.by] == s, ]
-            plots[[s]] <- pm(data = data, cols = cols, title = s, legend_title = signature)
-        }
-    }
-    return((patchwork::wrap_plots(plots, ncol = ncol) | legend) + patchwork::plot_layout(widths = c(3,
-        1)))
+  }
+  return((patchwork::wrap_plots(plots, ncol = ncol) | legend) + 
+          patchwork::plot_layout(widths = c(3, 1)))
 }
